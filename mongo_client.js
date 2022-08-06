57f4ff503d78115ae1ce6ad906411fdc439692fe
PreetamKumari <55556846+PreetamKumari@users.noreply.github.com>
2022-08-07 01:07:05 +0530

    Add files via upload

--- mongo_client.js
+++ mongo_client.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection;
+let uri = 'mongodb+srv://preetam:got_api123@cluster0.hullpjm.mongodb.net/?retryWrites=true&w=majority'
 module.exports = {
 	connectToDb: (cb) => {
-		MongoClient.connect('mongodb://localhost:27017/game_of_thrones')
+		MongoClient.connect(uri)
 		.then((client) => {
 			dbConnection = client.db()
 			return cb()
